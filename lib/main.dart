4ec24eac158a3ffa9cd7fc33e0756cad33e20ecb
Felipe Coppola <luizfelipecoppola@gmail.com>
2022-04-04 13:28:02 -0300

    Static ID Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+const primaryColor = Colors.deepPurpleAccent;
+
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+        theme: ThemeData(
+          fontFamily: 'Rubik',
+        ),
+      ),
+    );
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,32 +17,93 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: const Text(
-          'First App',
+          'ID Card',
         ),
         centerTitle: true,
-        backgroundColor: Colors.deepPurpleAccent,
+        backgroundColor: primaryColor,
+        elevation: 0.0,
       ),
-      body: const Center(
-        child: Text(
-          'hello world',
-          style: TextStyle(
-            fontSize: 25.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 1.5,
-            color: Colors.grey,
-            fontFamily: 'Rubik',
-          ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/codding.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            const Divider(
+              height: 30.0,
+              color: Colors.white,
+            ),
+            const Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 1.5,
+              ),
+            ),
+            const SizedBox(
+              height: 5.0,
+            ),
+            const Text(
+              'Felipe Coppola',
+              style: TextStyle(
+                  color: primaryColor,
+                  letterSpacing: 1.5,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Text(
+              'Current Age',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 1.5,
+              ),
+            ),
+            const SizedBox(
+              height: 5.0,
+            ),
+            const Text(
+              '25',
+              style: TextStyle(
+                  color: primaryColor,
+                  letterSpacing: 1.5,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: const [
+                Icon(
+                  Icons.email,
+                  color: primaryColor,
+                ),
+                SizedBox(
+                  width: 15.0,
+                ),
+                Text(
+                  'luizfelipecoppola@gmail.com',
+                  style: TextStyle(
+                      color: Colors.grey,
+                      letterSpacing: 1.5,
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Text('+',
-            style: TextStyle(
-              fontSize: 25.0,
-            )),
-        backgroundColor: Colors.deepPurpleAccent,
       ),
     );
   }
